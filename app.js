016d91c95ef31ae6a4693fe4105dca00b82f58c6
Buck Doyle <b@chromatin.ca>
2020-09-01 10:18:54 -0500

    Add proxying of location API

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hello' });
+const request = require('superagent');
+const apiKey = process.env['WINNIPEG_TRANSIT_API_KEY'] || 'API_KEY';
+
+app.get('/:query', async ({ params: query }, res) => {
+  const response = await request
+    .get(`https://api.winnipegtransit.com/v3/locations:${query.query}.json?api-key=${apiKey}`)
+    .set('accept', 'json');
+
+  const location = JSON.parse(response.text).locations[0];
+  const address = `${location['street-number']} ${location.street.name}`;
+
+  res.json({ address });
 });
 
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -2,8 +2,13 @@ const app = require('./app');
 const supertest = require('supertest');
 const request = supertest(app);
 
-it('responds', async done => {
-  const response = await request.get('/');
-  expect(response.body.message).toBe('hello');
+const nock = require('nock');
+nock('https://api.winnipegtransit.com')
+  .get('/v3/locations:510%20main.json?api-key=API_KEY')
+  .reply(200, {"locations":[{"key":151168,"street-number":510,"street":{"key":2265,"name":"Main Street","type":"Street"},"centre":{"utm":{"zone":"14U","x":633652,"y":5529177},"geographic":{"latitude":"49.90004","longitude":"-97.1389"}},"type":"address"}]});
+
+it('responds to a location query', async done => {
+  const response = await request.get('/510 main');
+  expect(response.body.address).toBe('510 Main Street');
   done();
 });
