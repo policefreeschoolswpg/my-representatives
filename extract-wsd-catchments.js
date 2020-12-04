cafb8892a522a37925c188bfbf94bfd4c20642fb
Buck Doyle <b@chromatin.ca>
2020-12-03 18:00:19 -0600

    Change extraction to map to districts

--- extract-wsd-catchments.js
+++ extract-wsd-catchments.js
@@ -1,6 +1,8 @@
 const parse = require('csv-parse/lib/sync');
 const fs = require('fs');
 
+const highSchoolToDistrict = JSON.parse(fs.readFileSync('data/wsd-high-school-to-district.json'));
+
 const data = parse(
   fs.readFileSync('data/wsd-postal-codes.csv'),
   {
@@ -12,8 +14,10 @@ const data = parse(
 const rowsWithPostalCodes = data.filter( ([ , , , possibleCode]) => possibleCode.match(/^\w\d\w \d\w\d$/));
 
 const postalCodeToHighSchool = rowsWithPostalCodes.reduce((postalCodeToHighSchool, row) => {
-  postalCodeToHighSchool[row[3]] = row[7];
+  const school = row[7];
+  const district = highSchoolToDistrict[school];
+  postalCodeToHighSchool[row[3]] = district;
   return postalCodeToHighSchool;
 }, {});
 
-fs.writeFileSync('data/wsd-postal-code-to-high-school.json', JSON.stringify(postalCodeToHighSchool, null, 2));
+fs.writeFileSync('data/wsd-postal-code-to-district.json', JSON.stringify(postalCodeToHighSchool, null, 2));
